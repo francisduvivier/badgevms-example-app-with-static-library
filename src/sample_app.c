3ede4fff5224b439286d2e7c952fc9a22f312c9a
Francis Duvivier <francisduvivier@gmail.com>
2025-08-06 19:17:50 +0200

    WORKING BADGEVMS APP!!!

--- src/sample_app.c
+++ src/sample_app.c
@@ -1,36 +1,36 @@
-#include "badgevms/process.h"
-
-#include <stdbool.h>   // for bool type
-#include <stdio.h>
-#include <stdlib.h> // for atoi
-
-#include <time.h>
+// #include "badgevms/process.h"
+//
+// #include <stdbool.h>   // for bool type
+// #include <stdio.h>
+// #include <stdlib.h> // for atoi
+//
+// #include <time.h>
 #include <unistd.h> // for usleep
 
-// This is the corrected macro definition.
-// Parentheses are added as a good practice for macros.
 #define ONE_SECOND_IN_USEC (1000 * 1000)
+//
+// // The current version of this application for printing to the console
+// const int CURRENT_VERSION = 1;
+// time_t process_start_time;
 
-// The current version of this application for printing to the console
-const int CURRENT_VERSION = 1;
-time_t process_start_time;
+// void version_logger_thread(void *data) {
+//     while (true) {
+//         printf("SAMPLE SUBPROCESS: APP V[%d] started at[%ld]\n", CURRENT_VERSION, (long)process_start_time);
+//         usleep(5 * ONE_SECOND_IN_USEC);
+//     }
+// }
 
-void version_logger_thread(void *data) {
-    while (true) {
-        printf("SAMPLE SUBPROCESS: APP V[%d] started at[%ld]\n", CURRENT_VERSION, (long)process_start_time);
-        usleep(5 * ONE_SECOND_IN_USEC);
-    }
-}
+#include "stdio.h"
 
 int main(int argc, char *argv[]) {
-    process_start_time = time(NULL);
-    printf("The current process timestamp is: %ld\n", (long)process_start_time);
+    // process_start_time = time(NULL);
+    // printf("The current process timestamp is: %ld\n", (long)process_start_time);
+    //
+    // thread_create(version_logger_thread, NULL, 4096);
 
-    thread_create(version_logger_thread, NULL, 4096);
-
-    while (true) {
+    while (1) {
         usleep(ONE_SECOND_IN_USEC);
-        printf("SAMPLE MAIN: APP V[%d] started at[%ld]\n", CURRENT_VERSION, (long)process_start_time);
+        printf("SAMPLE MAIN: APP V[%d] started at[%ld]\n", 0, (long)0);
     }
     return 0;
 }

614f241735d7f9917e0ab707ce7dd33148c5b7b3
Francis Duvivier <francisduvivier@gmail.com>
2025-08-06 09:03:21 +0200

    Sample app was completely crashing the badge. Try fix it

--- src/sample_app.c
+++ src/sample_app.c
@@ -1,34 +1,36 @@
 #include "badgevms/process.h"
 
-#include <stdatomic.h> // for atomic variables
 #include <stdbool.h>   // for bool type
 #include <stdio.h>
 #include <stdlib.h> // for atoi
 
 #include <time.h>
 #include <unistd.h> // for usleep
-#define ONE_SECOND_IN_USEC 1000 * 1000
+
+// This is the corrected macro definition.
+// Parentheses are added as a good practice for macros.
+#define ONE_SECOND_IN_USEC (1000 * 1000)
 
 // The current version of this application for printing to the console
-int const CURRENT_VERSION = 1;
-int         process_start_time = -1;
-time_t unix_timestamp;
+const int CURRENT_VERSION = 1;
+time_t process_start_time;
 
 void version_logger_thread(void *data) {
     while (true) {
-        printf("SAMPLE SUBPROCESS: APP V[%d] started at[%ld], ", process_start_time, (long)unix_timestamp);
+        printf("SAMPLE SUBPROCESS: APP V[%d] started at[%ld]\n", CURRENT_VERSION, (long)process_start_time);
         usleep(5 * ONE_SECOND_IN_USEC);
     }
 }
 
 int main(int argc, char *argv[]) {
-    unix_timestamp = time(NULL);
-    printf("The current process timestamp is: %ld\n", (long)unix_timestamp);
+    process_start_time = time(NULL);
+    printf("The current process timestamp is: %ld\n", (long)process_start_time);
+
     thread_create(version_logger_thread, NULL, 4096);
 
     while (true) {
-
         usleep(ONE_SECOND_IN_USEC);
-        printf("SAMPLE MAIN: APP V[%d] started at[%ld], ", process_start_time, (long)unix_timestamp);
+        printf("SAMPLE MAIN: APP V[%d] started at[%ld]\n", CURRENT_VERSION, (long)process_start_time);
     }
+    return 0;
 }
